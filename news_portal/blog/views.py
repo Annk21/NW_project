e79e65673a8a383ea85db7ed6c20301c74e9781c
Алина Неустроева <alina@Noutbuk-Alina.local>
2024-03-15 16:03:13 +0500

    second commit

--- news_portal/blog/views.py
+++ news_portal/blog/views.py
@@ -1,7 +1,10 @@
+from django.urls import reverse_lazy
 from datetime import datetime
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
 
 
 class PostsList(ListView):
@@ -9,12 +12,24 @@ class PostsList(ListView):
     ordering = 'date_time'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 10
 
 
-    def get_context_data(self, **kwargs):
+class SearchPosts(ListView):
+    paginate_by = 10
+    model = Post
+    ordering = 'date_time'
+    template_name = 'search.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+    
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        context['all_news'] = None
+        context['search_filter'] = self.filterset
         return context
 
 
@@ -24,4 +39,74 @@ class PostDetail(DetailView):
     context_object_name = 'post'
 
 
+class ArticleCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
 
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Добавить статью"
+        return context
+
+
+class ArticleUpdate(UpdateView):
+    """ Представление для редактирования статьи. """
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Редактировать статью"
+        return context
+
+
+class ArticleDelete(DeleteView):
+    """ Представление для удаления статьи. """
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Удалить статью"
+        context['previous_page_url'] = reverse_lazy('posts_list')
+        return context
+
+
+class NewsCreate(CreateView):
+    """ Представление для создания новости. """
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Добавить новость"
+        return context
+
+
+class NewsUpdate(UpdateView):
+    """ Представление для редактирования новости. """
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Редактировать новость"
+        return context
+
+
+class NewsDelete(DeleteView):
+    """ Представление для удаления новости. """
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Удалить новость"
+        context['previous_page_url'] = reverse_lazy('posts_list')
+        return context
